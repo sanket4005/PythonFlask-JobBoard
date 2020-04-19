7c5feab33829f9f208891bb7ecfb13cbd64304ff
Sanket Naik <mail@sanketnaik.in>
2020-04-20 01:57:08 +0530

    Completed module

--- jobs/app.py
+++ jobs/app.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@route('/')
-@route('/jobs')
+@app.route('/')
+@app.route('/jobs')
 def jobs():
     return render_template('index.html')
